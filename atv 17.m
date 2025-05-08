49ae0b54057fefbde2cc19a1c27f7a68a26b96eb
julianopls <brolezzijuliano@gmail.com>
2025-05-08 13:29:48 -0300

    atv 17.m

---
+++ atv 17.m
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+struct Pessoa {
+    char nome[50];
+    int idade;
+};
+int main() {
+    struct Pessoa pessoa1, pessoa2;
+    
+    //os dados da primeira pessoa
+    printf("Digite o nome da primeira pessoa:\n");
+    scanf("%49s", pessoa1.nome); //esse 49s e para ler até 49 letras
+    printf("Digite a idade da primeira pessoa:\n");
+    scanf("%d", &pessoa1.idade);
+    
+    //dados da segunda pessoa
+    printf("Digite o nome da segunda pessoa:\n");
+    scanf("%49s", pessoa2.nome); //esse 49s e para ler até 49 letras
+    printf("Digite a idade da segunda pessoa:\n");
+    scanf("%d", &pessoa2.idade);
+    
+    //aqui compara as idades e informa quem e mais velho
+    if (pessoa1.idade > pessoa2.idade) 
+	{
+    printf("%s é mais velho que %s.\n", pessoa1.nome, pessoa2.nome);
+    } 
+	else if (pessoa1.idade < pessoa2.idade) {
+    printf("%s e mais velho que %s.\n", pessoa2.nome, pessoa1.nome);
+    } 
+	else
+	{
+    printf("%s e %s tem a mesma idade.\n", pessoa1.nome, pessoa2.nome);
+    }
+    return 0;
+}
